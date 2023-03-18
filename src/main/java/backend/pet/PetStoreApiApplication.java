6a584dfc5456c213d5f05c655b276b84339a65c8
adi247u <adi247u@gmail.com>
2023-03-19 00:12:19 +0530

    Blank Project Created

---
+++ src/main/java/backend/pet/PetStoreApiApplication.java
@@ -0,0 +1,13 @@
+package backend.pet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetStoreApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetStoreApiApplication.class, args);
+	}
+
+}
